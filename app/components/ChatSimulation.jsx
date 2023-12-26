b70d8f3f09f3f2bc2f458b4fc74bba39f0638e86
Nick Littman <nlittman@Nicks-Laptop.localdomain>
2023-12-26 11:42:29 -0500

    moment toast styling

--- app/components/ChatSimulation.jsx
+++ app/components/ChatSimulation.jsx
@@ -8,14 +8,13 @@ import {
 import "../globals.css";
 import { FaGithub } from "react-icons/fa";
 
-
 const ChatSimulation = () => {
   const [message, setMessage] = useState("");
   const [messageRate, setMessageRate] = useState(5);
   const [simulationState, setSimulationState] = useState("stopped"); // 'stopped', 'running', 'paused'
   const [toastMessage, setToastMessage] = useState("");
   const [showToast, setShowToast] = useState(false); // State for controlling the toast visibility
-  const [showMoment, setShowMoment] = useState(true); // State for controlling the moment visibility
+  const [showMomentButton, setShowMomentButton] = useState(true); // State for controlling the moment visibility
 
   const chatWindowRef = useRef(null);
   const messageIntervalRef = useRef(null);
@@ -89,9 +88,11 @@ const ChatSimulation = () => {
       appendMessage(generateRandomMessage());
     }, intervalTime);
 
-    setToastMessage(`⭐⭐⭐Big moment! Message Rate Spiked to ${5 * previousRate} mps⭐⭐⭐`);
+    setToastMessage(
+      `⭐⭐⭐Big moment! Message Rate Spiked to ${5 * previousRate} mps⭐⭐⭐`
+    );
     setShowToast(true);
-    setShowMoment(false);
+    setShowMomentButton(false);
 
     // Restore the previous message rate after 2 seconds
     setTimeout(() => {
@@ -104,7 +105,7 @@ const ChatSimulation = () => {
         }, 1000 / previousRate);
       }
       setShowToast(false);
-      setShowMoment(true);
+      setShowMomentButton(true);
     }, 2000);
   };
 
@@ -123,95 +124,106 @@ const ChatSimulation = () => {
 
   return (
     <div className="container mx-auto px-2 sm:px-4 py-2 dark:bg-gray-800">
-  <div className="flex flex-col justify-center items-center space-y-4">
-    <div className="w-full sm:w-2/3 lg:w-1/2">
-      {showToast && (
-        <div className="toast mb-4 text-center text-sm bg-gray-200 dark:bg-gray-700 dark:text-white rounded">
-          {toastMessage}
-        </div>
-      )}
-      <div ref={chatWindowRef} className="overflow-y-auto h-96 mb-4 bg-white dark:bg-gray-900 rounded-lg p-4">
-        {/* Chat bubbles will be appended here */}
-      </div>
-      <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row md:items-center md:space-x-4">
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          onKeyDown={handleKeyDown}
-          className="flex-grow bg-gray-100 dark:bg-gray-700 dark:text-white rounded p-2"
-          placeholder="Type your message..."
-        />
-        <button
-          onClick={handleSendMessage}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition-colors"
-        >
-          Send
-        </button>
-      </div>
-      <div className="mt-4">
-        <label className="font-semibold text-gray-800 dark:text-gray-200">
-          Message rate:
-        </label>
-        <div className="flex items-center mt-1">
-          <input
-            type="range"
-            min="1"
-            max="200"
-            value={messageRate}
-            onChange={(e) => setMessageRate(e.target.value)}
-            className="slider flex-grow"
-          />
-          <input
-            type="number"
-            min="1"
-            max="200"
-            value={messageRate}
-            onChange={(e) => setMessageRate(e.target.value)}
-            className="ml-2 border border-gray-300 dark:border-gray-600 dark:text-gray-700 rounded p-1 text-center w-16"
-          />
-          <span className="ml-2 text-gray-500 dark:text-gray-400 text-sm">
-            messages per second
-          </span>
-        </div>
-      </div>
-      <div className="flex flex-wrap justify-center md:justify-start gap-2 mt-4">
-        {simulationState === "running" && showMoment && (
-          <button
-            onClick={handleCreateMoment}
-            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full transition-colors"
+      <div className="flex flex-col justify-center items-center space-y-4">
+        <div className="w-full sm:w-2/3 lg:w-1/2">
+          {showToast && (
+            <div className="toast mb-4 text-center text-sm bg-gray-200 dark:bg-gray-700 dark:text-white rounded">
+              <span className="rainbow-text" style={{ fontSize: "20px" }}>
+                {toastMessage}
+              </span>
+            </div>
+          )}
+
+          <div
+            ref={chatWindowRef}
+            className="overflow-y-auto h-96 mb-4 bg-white dark:bg-gray-900 rounded-lg p-4"
           >
-            Create Moment
-          </button>
-        )}
-        <button
-          onClick={simulationState === "running" ? handlePauseResume : startSimulation}
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full transition-colors"
-        >
-          {simulationState === "running" ? "Pause" : "Start"} Simulation
-        </button>
-        <button
-          onClick={handleStopSimulation}
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full transition-colors"
-        >
-          Stop Simulation
-        </button>
+            {/* Chat bubbles will be appended here */}
+          </div>
+          <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row md:items-center md:space-x-4">
+            <input
+              type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+              className="flex-grow bg-gray-100 dark:bg-gray-700 dark:text-white rounded p-2"
+              placeholder="Type your message..."
+            />
+            <button
+              onClick={handleSendMessage}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition-colors"
+            >
+              Send
+            </button>
+          </div>
+          <div className="mt-4">
+            <label className="font-semibold text-gray-800 dark:text-gray-200">
+              Message rate:
+            </label>
+            <div className="flex items-center mt-1">
+              <input
+                type="range"
+                min="1"
+                max="200"
+                value={messageRate}
+                onChange={(e) => setMessageRate(e.target.value)}
+                className="slider flex-grow"
+              />
+              <input
+                type="number"
+                min="1"
+                max="200"
+                value={messageRate}
+                onChange={(e) => setMessageRate(e.target.value)}
+                className="ml-2 border border-gray-300 dark:border-gray-600 dark:text-gray-700 rounded p-1 text-center w-16"
+              />
+              <span className="ml-2 text-gray-500 dark:text-gray-400 text-sm">
+                messages per second
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-wrap justify-center md:justify-start gap-2 mt-4">
+            {simulationState === "running" && showMomentButton && (
+              <button
+                onClick={handleCreateMoment}
+                className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full transition-colors"
+              >
+                Create Moment
+              </button>
+            )}
+            {showMomentButton && (
+              <div>
+                <button
+                  onClick={
+                    simulationState === "running"
+                      ? handlePauseResume
+                      : startSimulation
+                  }
+                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full transition-colors"
+                >
+                  {simulationState === "running" ? "Pause" : "Start"} Simulation
+                </button>
+                <button
+                  onClick={handleStopSimulation}
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full transition-colors"
+                >
+                  Stop Simulation
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
+      <a
+        href="https://github.com/NickLittman/chat-simulation"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Fork me on GitHub"
+        className="text-gray-800 dark:text-white hover:text-blue-600 dark:hover:text-blue-400 mt-4"
+      >
+        <FaGithub className="text-4xl" />
+      </a>
     </div>
-  </div>
-  <a
-    href="https://github.com/NickLittman/chat-simulation"
-    target="_blank"
-    rel="noopener noreferrer"
-    title="Fork me on GitHub"
-    className="text-gray-800 dark:text-white hover:text-blue-600 dark:hover:text-blue-400 mt-4"
-  >
-    <FaGithub className="text-4xl" />
-  </a>
-</div>
-
-
-
   );
 };
 
